b7ca39f004753e0ac3e8acbcefba5801b958113f
szenius <ting.szeying@gmail.com>
2015-09-19 15:50:09 +0800

    added unit tests for add, delete, display, clear

--- src/TextBuddyTest.java
+++ src/TextBuddyTest.java
@@ -2,103 +2,56 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextBuddyTest {
 	
-	@Test
-	public void testAddText() {
-		TextBuddy bud = new TextBuddy();
-		String message = new String();
-		
-		try {
-			message = bud.testExecuteCommand("add little brown fox");
-			assertEquals(message, "added to test.txt: \"little brown fox\"");
-			
-			message = bud.testExecuteCommand("add mary had a little lamb");
-			assertEquals(message, "added to test.txt: \"mary had a little lamb\"");
+	private TextBuddy buddy;
 	
-			message = bud.testExecuteCommand("add merry merry ho ho ho");
-			assertEquals(message, "added to test.txt: \"merry merry ho ho ho\"");
-			
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
+	@Before
+	public void testSetup() throws IOException{
+		buddy = new TextBuddy("testFile.txt");
 	}
-	
+
 	@Test
-	public void testDeleteText() {
-		TextBuddy bud = new TextBuddy();
-		String message = new String();
-		
-		try {
-			bud.testExecuteCommand("add pppppppapokerface");
-			bud.testExecuteCommand("add mamaramama");
-			bud.testExecuteCommand("add just dance");
-	
-			message = bud.testExecuteCommand("delete 1");
-			assertEquals(message, "deleted from test.txt: \"pppppppapokerface\"");
-
-			message = bud.testExecuteCommand("delete 2");
-			assertEquals(message, "deleted from test.txt: \"just dance\"");
-
-			message = bud.testExecuteCommand("delete 1");
-			assertEquals(message, "deleted from test.txt: \"mamaramama\"");
-						
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
+	public void testAddText() throws IOException{
+		assertEquals(buddy.executeCommand("add one little plankton"), 
+					"added to testFile.txt: \"one little plankton\"");
+		assertEquals(buddy.executeCommand("add two little giants"), 
+				"added to testFile.txt: \"two little giants\"");
+		assertEquals(buddy.executeCommand("add three little daisies"), 
+				"added to testFile.txt: \"three little daisies\"");
 
 	}
 	
 	@Test
-	public void testClearAllText() {
-		TextBuddy bud = new TextBuddy();
-		String message = new String();
-		
-		try {
-			bud.testExecuteCommand("add we the citizens");
-			bud.testExecuteCommand("add of singapore");
-			bud.testExecuteCommand("add pledge ourselves");
-			bud.testExecuteCommand("add to be one united people");
-	
-			message = bud.testExecuteCommand("clear");
-			assertEquals(message, "all content deleted from test.txt");	
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
+	public void testDeleteText() throws IOException{
+		testAddText();
+		assertEquals(buddy.executeCommand("delete 1"), 
+					"deleted from testFile.txt: \"one little plankton\"");
+		assertEquals(buddy.executeCommand("delete 2"), 
+				"deleted from testFile.txt: \"three little daisies\"");
+		assertEquals(buddy.executeCommand("delete 1"), 
+				"deleted from testFile.txt: \"two little giants\"");
 	}
 	
 	@Test
-	public void testDisplayText() {
-		TextBuddy bud = new TextBuddy();
-		String message = new String();
+	public void testClearAllText() throws IOException{
+		testAddText();
+		assertEquals(buddy.executeCommand("clear"), 
+					"all content deleted from testFile.txt");
+	}
+	
+	@Test
+	public void testDisplayText() throws IOException{
+		testAddText();
 		
-		try {
-			bud.testExecuteCommand("add we are never ever ever");
-			message = bud.testExecuteCommand("display");
-			assertEquals(message, "1. we are never ever ever");
-			
-			bud.testExecuteCommand("add ever ever ever");
-			bud.testExecuteCommand("add ever ever ever");
-			message = bud.testExecuteCommand("display");
-			assertEquals(message, "1. we are never ever ever\n2. ever ever ever\n 3. ever ever ever");
-	
-			bud.testExecuteCommand("delete 2");
-			message = bud.testExecuteCommand("display");
-			assertEquals(message, "1. we are never ever ever\n2. ever ever ever");
-	
-			bud.testExecuteCommand("add getting back together");
-			message = bud.testExecuteCommand("display");
-			assertEquals(message, "1. we are never ever ever\n2. ever ever ever\n3. getting back together");
-	
-			bud.testExecuteCommand("clear");
-			message = bud.testExecuteCommand("display");
-			assertEquals(message, "test.txt is empty");		
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
+		String expectedDisplayText = "1. one little plankton\n";
+		expectedDisplayText += "2. two little giants\n";
+		expectedDisplayText += "3. three little daisies";
 		
+		assertEquals(buddy.executeCommand("display"), expectedDisplayText);
 	}
 
 }
